f30d59fd2ab58990db8d092ccccdbfc091dbea9e
Jillian0210 <18010779558@163.com>
2022-05-30 20:33:56 +0800

    命名空间

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 
+namespace namespaceA {
+    int a;
+
+    void fun();
+
+    namespace namespaceB {
+        int a;
+
+        void fun();
+
+        namespace namespaceC {
+            int a;
+        }
+    }
+}
+namespace namespaceAA {
+    int a;
+}
+
+using namespace namespaceA;
+using namespace namespaceAA;
+
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << "Hello, World!" << std::endl;
+
+//endl 是换行
+cout<<"hello,world"<<endl;
+//cin>>
+
+
+    int x = namespaceA::a;
+    int y = namespaceA::namespaceB::namespaceC::a;
+    int z = namespaceAA::a;
+    a;
+
+    a;
+
     return 0;
 }
