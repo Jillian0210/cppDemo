0dd20f64b701c12b45c9b09ae89e721334963a0f
Jillian0210 <18010779558@163.com>
2022-06-01 19:04:45 +0800

    使用#ifndef...#define...#endif... 头文件防止重复引入

---
+++ Car.cpp
@@ -0,0 +1,11 @@
+//
+// Created by jiangli on 2022/6/1.
+//
+
+
+#include "Car.h"
+#include <iostream>
+
+void Car::run() {
+    std::cout<<"run";
+}
---
+++ Car.h
@@ -0,0 +1,18 @@
+//
+// Created by jiangli on 2022/6/1.
+//
+
+#ifndef CPPDEMO_CAR_H
+#define CPPDEMO_CAR_H
+
+
+class Car {
+
+public:
+    char * color;
+    int price;
+    void run();
+};
+
+
+#endif //CPPDEMO_CAR_H
